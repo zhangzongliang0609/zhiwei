cf04635d6e22e89cf8acf7ad02db622456b43ae8
zhangzongliang0609 <1628930438@qq.com>
2019-08-15 19:35:34 +0800

    ddd

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+   <h1>你好</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
